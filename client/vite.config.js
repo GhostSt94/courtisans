f8d491c8442074ed90ef1dacd5572d59ee6bee98
Reda <reda.benammi94@gmail.com>
2026-02-22 03:51:39 +0000

    feat: add backend server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'http://localhost:3000',
+        target: 'https://courtisans-e6kg.onrender.com',
         ws: true
       }
     }
