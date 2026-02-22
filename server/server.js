2c77b738e4a75062f8462d18622d446a2972b43b
Reda <reda.benammi94@gmail.com>
2026-02-22 04:12:14 +0000

    feat: server add PORT in .env

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const GameManager = require("./game/GameManager");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(cors());
 
@@ -12,8 +14,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173"
-  },
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
 const gameManager = new GameManager();
@@ -130,6 +133,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
